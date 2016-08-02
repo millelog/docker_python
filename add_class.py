edf6ec6df51cc0a10b83fd0aadc66210f541573f
millelog <millerl1960@gmail.com>
2016-08-02 15:59:50 -0700

    Changed primary key of port table

--- add_class.py
+++ add_class.py
@@ -4,36 +4,32 @@ import argparse
 
 def parse_args():
         parser = argparse.ArgumentParser(description='Create a new dockerized jupyterhub teaching interface')
-        parser.add_argument('-n', dest='class_name', required=True,                                                                                                                                            
-                help = "Name of the class")                                                                                                                                                                    
-        parser.add_argument('-f', dest='first', required=True,                                                                                                                                                 
-                help = "Instructor's first name")                                                                                                                                                              
-        parser.add_argument('-l', dest='last', required=True,                                                                                                                                                  
-                help = "Instrucotr's last name")                                                                                                                                                               
-        parser.add_argument('-e', dest='email', required=True,                                                                                                                                                 
-                help = "Instructor's email")                                                                                                                                                                   
-        parser.add_argument('-u', dest='user', required=True,                                                                                                                                                  
-                help = "Instructor's username")                                                                                                                                                                
-        parser.add_argument('-p', dest='port', required=True,                                                                                                                                                  
-                help = "Host's port to map the docker to.")                                                                                                                                                    
-        parser.add_argument('-v', dest='version', required=True,                                                                                                                                               
-                help="Version number of the jupyterhub/cgrb image.")                                                                                                                                           
-        return parser.parse_args()                                                                                                                                                                             
-                                                                                                                                                                                                               
-def create_class(args):                                                                                                                                                                                        
-        cli = Client(base_url='unix://var/run/docker.sock')                                                                                                                                                    
-        container = cli.create_container(image='jupyterhub/cgrb:v'+args.version,                                                                                                                               
-                        command='/bin/bash',                                                                                                                                                                   
-                        name=args.class_name, ports=[8000],                                                                                                                                                    
-                        host_config=cli.create_host_config(port_bindings={8000:80}),                                                                                                                           
+        parser.add_argument('-n', dest='class_name', required=True,
+                help = "Name of the class")
+        parser.add_argument('-f', dest='first', required=True,
+                help = "Instructor's first name")
+        parser.add_argument('-l', dest='last', required=True,
+                help = "Instrucotr's last name")
+        parser.add_argument('-e', dest='email', required=True,
+                help = "Instructor's email")
+        parser.add_argument('-u', dest='user', required=True,
+                help = "Instructor's username")
+        parser.add_argument('-p', dest='port', required=True,
+                help = "Host's port to map the docker to.")
+        parser.add_argument('-v', dest='version', required=True,
+                help="Version number of the jupyterhub/cgrb image.")
+        return parser.parse_args()
+
+def create_class(args):
+        cli = Client(base_url='unix://var/run/docker.sock')
+        container = cli.create_container(image='jupyterhub/actf:v'+args.version,
+                        command='/bin/bash',
+                        name=args.class_name, ports=[8000],
+                        host_config=cli.create_host_config(port_bindings={8000:80}),
                         volumes= ['/home','/srv/cgrb'], stdin_open=True, detach=True)
 
         response = cli.start(container=container.get('Id'))
         print(response)
-
-        #initialize = cli.exec_create(container=args.class_name, cmd = '/srv/initialize_jupyterhub.sh')
-        #response = cli.exec_start(initialize.get('Id'))
-        #print(response)
 
         create_user = cli.exec_create(container=args.class_name, cmd = '/opt/anaconda/bin/python /home/jupyter_python/manage_users.py -a '+args.first+' '+args.last+' '+args.user+' '+args.email+' instructor True')
         response = cli.exec_start(create_user.get('Id'))
