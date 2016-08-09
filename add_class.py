8bb78a0e124cb92eef49c380ffb4010447237e86
millelog <millerl1960@gmail.com>
2016-08-09 12:58:46 -0700

    split up functions and added nginx config

--- add_class.py
+++ add_class.py
@@ -22,33 +22,54 @@ def parse_args():
 
 def create_class(args):
         cli = Client(base_url='unix://var/run/docker.sock')
-        container = cli.create_container(image='jupyterhub/actf:v'+args['version'],
+        #Create a container that mounts the given volumes and ports and sets resource limits
+        container = cli.create_container(image='jupyterhub/actf:'+args['version'],
                         command='/bin/bash',
                         name=args['class_name'], ports=[8000],
                         volumes= ['/home','/srv/cgrb', '/local/cluster'],
-                        host_config=cli.create_host_config(port_bindings={8000:args['port']}, binds={
-                                '/local/cluster':{
-                                        'bind': '/local/cluster',
-                                        'mode': 'ro',
-                                }
-                        }),
-                        stdin_open=True, detach=True)
+                        host_config=cli.create_host_config(
+                                port_bindings={8000:args['port']}, 
+                                binds={
+                                        '/local/cluster':{
+                                                'bind': '/local/cluster',
+                                                'mode': 'ro',
+                                        }
+                                }, mem_limit=args['mem_limit']
+                        ), stdin_open=True, detach=True)
 
         response = cli.start(container=container.get('Id'))
         print(response)
 
+        #Run the command and print the response returned, Creates initial user
+        print(create_instrucotr(cli, args))
+
+        #Run the command to add a line to the jupytehrub config file and print response
+        config_line = 'c.Application.base_url = \'/'+args['class_name']+'/\''
+        print(add_to_config(cli, args['class_name'], config_line)) 
+
+        #Run the command to start the server and print the response
+        print(start_jupyterhub(cli, args['class_name'])
+
+        #Commit the class creation to database
+        database_commit(args['port'], args['class_name'], args['user'])
+
+def start_jupyterhub(cli, container_name):
+        class_start = cli.exec_create(container=args['class_name'], cmd = 'jupyterhub --no-ssl')
+        return(cli.exec_start(class_start.get('Id'), detach=True))
+
+def add_to_config(cli, container_name, config_line):
+        config_line = 'c.Application.base_url = \'/'+container_name+'/\''
+        edit_config = cli.exec_create(container=container_name,
+                cmd = 'echo '+config_line+' >> /srv/jupyterhub_config.py')
+        return cli.exec_start(edit_config.get('Id'))
+
+
+def create_instructor(cli, args):
         create_user = cli.exec_create(container=args['class_name'], cmd =\
          '/opt/anaconda/bin/python /home/public/data/jupyter_python/manage_users.py -a '+\
         args['first']+' '+args['last']+' '+args['user']+' '+args['email']+' instructor True')
-        
-        response = cli.exec_start(create_user.get('Id'))
-        print(response)
+        return cli.exec_start(create_user.get('Id'))
 
-        class_start = cli.exec_create(container=args['class_name'], cmd = 'jupyterhub --no-ssl')
-        response = cli.exec_start(class_start.get('Id'), detach=True)
-        print(response)
-
-        database_commit(args['port'], args['class_name'], args['user'])
 
 def delete_class(name):
     cli = Client(base_url='unix://var/run/docker.sock')
@@ -79,11 +100,7 @@ def valid_parser(args):
            valid_input(args.version)
 
 def main():
-        args = parse_args()
-        if(valid_parser(args)):
-                create_class(args)
-        else:
-                print("Invalid command line inputs, please only use valid characters.")
+        print("Depreciated by manage_class.py")
 
 if __name__=='__main__':
         main()
