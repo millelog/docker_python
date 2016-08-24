e6d17d1bd3af2de6341aed5562f30e8c4b1a098f
Logan H. Miller <millerl1960@gmail.com>
2016-08-24 13:48:57 -0700

    Update class_database.py

--- class_database.py
+++ class_database.py
@@ -8,6 +8,7 @@ import logger as log
 class class_database(object):
         """This class will do all read and writes to the class database"""
         def __init__(self):
+                #Variables that refer to columns and names of the database to lower the chance of spelling errors
                 self.db_path = "/data/cgrb/database.sqlite"
                 self.ctn = 'classes'
                 self.ptn = 'ports'
@@ -49,6 +50,7 @@ class class_database(object):
 
         def populate_port_table(self, hv):
                 c = self.get_connection()
+                #Add 100 ports as not in use for the given host name
                 for i in range(8001, 8101):
                         sql="INSERT OR IGNORE INTO {ptn} VALUES ('{hv}', '{pv}', '{iuv}');".\
                                 format(ptn=self.ptn, hv=hv, pv=i, iuv=0)
@@ -83,6 +85,22 @@ class class_database(object):
                 log.log(" : added to database : "+class_name+" - Instructor: "+instructor+" - Host: "+host+" - Port: "+str(port))
                 #commit info to database
                 self.commit_db()
+        
+        def get_instructor_info(self, class_name):
+                '''Returns a dict of the isntructors first, last and email'''
+                c = self.get_connection()
+                #Define the dictionaries
+                info = {'first': '', 'last': '', 'email': ''}
+                #Returns a tuple of the columns notated where class name is the same as the parameter
+                for row in c.execute("SELECT {first}, {last}, {email} FROM {ctn} WHERE {cn} = \'{class_name}\';".\
+                        format(first='first', last='last', email='email', ctn=self.ctn, cn= self.class_name, class_name=class_name)):
+                        info = {
+                                'first': row[0].capitalize(),
+                                'last': row[1].capitalize(),
+                                'email': row[2]
+                        }
+                return info
+                 
 
         def get_class_names(self):
                 #connect to the database
@@ -111,19 +129,31 @@ class class_database(object):
                 #Get port of class_name
                 sql = "SELECT {p} FROM {ctn} WHERE {cn} = \'{name}\';".\
                         format(p=self.port, ctn=self.ctn, cn=self.class_name, name=class_name)
-                # execute returns a tuple or something hopefully first index is right
-                port = c.execute(sql).fetchone()[0]
-                log.log("Class name: "+class_name+"     Port: "+str(port))
+
+                # execute returns a tuple of tuples, turn it into a list 
+                ports = c.execute(sql).fetchall()
+                #If no available ports
+                if not ports:
+                        log.log("There are no avaibale ports for "+class_name+" and the program quit without finishing")
+                        exit(2)
+
+                remove_ports = list()
+                for port in ports:
+                        remove_ports.append(port[0])
+                for port in remove_ports:
+                        #command string to set port use table
+                        sql="UPDATE {ptn} SET {iu}= {iuv} WHERE {p} = {pv};".\
+                               format(ptn=self.ptn, iu=self.inuse, iuv=0, p=self.port, pv=port)
+                        c.execute(sql)
+                        #log this removal
+                        log.log(" : removed from database : "+class_name+" : "+str(port)+" is now free")
+                
                 #command string to delete from class table
                 sql = "DELETE FROM {ctn} WHERE {cn} = \'{name}\';".\
                         format(ctn=self.ctn, cn=self.class_name, name=class_name)
                 c.execute(sql)
-                #command string to set port use table
-                sql="UPDATE {ptn} SET {iu}= {iuv} WHERE {p} = {pv};".\
-                        format(ptn=self.ptn, iu=self.inuse, iuv=0, p=self.port, pv=port)
-                c.execute(sql)
-                #log this removal
-                log.log(" : removed from database : "+class_name+" : "+str(port)+" is now free")
+                
+                
                 #Commit to database
                 self.commit_db()
 
@@ -143,9 +173,15 @@ class class_database(object):
         def get_available_ports(self, host):
                 c=self.get_connection()
                 ports=[]
+                #Select ports of the given host name that are not in use
                 for row in c.execute("SELECT {port} FROM {ptn} WHERE {h} = '{hv}' AND {iu} = '{iuv}';".\
                 format(port=self.port, ptn=self.ptn, h='host', hv=host, iu=self.inuse, iuv=0)):
                         ports.append(row[0])
+
+                sql = 'UPDATE {ptn} SET {iu}={iuv} WHERE {p} = {pv};'.\
+                        format(ptn=self.ptn, iu=self.inuse, iuv=1, p=self.port, pv=ports[0])
+                c.execute(sql)
+                self.commit_db()
                 return ports
 
         def get_first_available_port(self, host):
